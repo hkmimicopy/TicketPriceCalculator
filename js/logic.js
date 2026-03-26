36885bdf99da2b6fc8bfb841a044ea1d127fd9f8
hkmimicopy <k_hattori@know-net.co.jp>
2026-03-26 15:49:06 +0900

    16-18で意図的にバグを仕込む

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,6 @@ export function calculatePrice(age) {
   if (age < 6) return 0;
   if (age <= 12) return 500;
   if (age <= 15) return 600;
-  if (age <= 18) return 800;
+  if (age <= 16) return 800;
   return 1000;
 }
